b9454b8472fa459291afd3cecdf613766f56a018
GiselleS23 <128999381+GiselleS23@users.noreply.github.com>
2024-05-03 14:56:42 +0000

    borre comentarios innecesarios

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,62 +5,29 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    
+
     Scanner scanner = new Scanner(System.in);
 
-    double precUnitProd=0;
-    /* double precUnitProd2;
-    double precUnitProd3;
-    double precUnitProd4; */
+    double precUnitProd = 0;
+    double cantProd = 0;
 
-    double cantProd=0;
-    
-    /* double cantProd2;
-    double cantProd3;
-    double cantProd4; */
+    double totalConsumo = 0;
+    double subtotal = 0;
 
-    double totalConsumo=0;
-    double subtotal=0;
-    
-    //le implemento for para pedirle solo 4 veces al usuario el precio unitario de cada       producto
-    for (int i = 0; i < 4; i++){
-      System.out.println("Ingrese el precio unitario del producto " + (i+1));
+    // le implemento for para pedirle solo 4 veces al usuario el precio unitario de
+    // cada producto
+    for (int i = 0; i < 4; i++) {
+      System.out.println("Ingrese el precio unitario del producto " + (i + 1));
       precUnitProd = scanner.nextDouble();
-      System.out.println("Ingrese la cantidad del producto :" + (i+1));
-      cantProd = scanner.nextDouble(); 
+      System.out.println("Ingrese la cantidad del producto :" + (i + 1));
+      cantProd = scanner.nextDouble();
 
-      subtotal=  precUnitProd * cantProd;
-      
+      subtotal = precUnitProd * cantProd;
+
       totalConsumo = totalConsumo + subtotal;
-      
     }
 
-    /* System.out.println("Ingrese el precio unitario del producto 1");
-    precUnitProd1 = scanner.nextDouble();
-    System.out.println("Ingrese la cantidad del producto 1");
-    cantProd1 = scanner.nextDouble();
-
-    System.out.println("Ingrese el precio unitario del producto 2");
-    precUnitProd2 = scanner.nextDouble();
-    System.out.println("Ingrese la cantidad del producto 2");
-    cantProd2 = scanner.nextDouble();
-
-    System.out.println("Ingrese el precio unitario del producto 3");
-    precUnitProd3 = scanner.nextDouble();
-    System.out.println("Ingrese la cantidad del producto 3");
-    cantProd3 = scanner.nextDouble();
-
-    System.out.println("Ingrese el precio unitario del producto 4");
-    precUnitProd4 = scanner.nextDouble();
-    System.out.println("Ingrese la cantidad del producto 4");
-    cantProd4 = scanner.nextDouble(); */
-
-
-/* totalConsumo = precUnitProd * cantProd; */
-    
-    /* totalConsumo = (precUnitProd1 * cantProd1) + (precUnitProd2 * cantProd2) + (precUnitProd3 * cantProd3)
-        + (precUnitProd4 * cantProd4); */
-    System.out.println("El total del consumo del cliente es: "+ totalConsumo);
+    System.out.println("El total del consumo del cliente es: " + totalConsumo);
     scanner.close();
   }
 
